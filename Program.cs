f2432b6dc6eb21a3b55d01be51f2d6db51d896e5
mythili annamalai <mythiliannamalai45@gmail.com>
2022-03-23 17:38:18 +0530

    Add_Node

--- Program.cs
+++ Program.cs
@@ -9,10 +9,12 @@ namespace Datastructure
             Node node1 = new Node(22);
             Node node2 = new Node(23);
             Node node3 = new Node(24);
-            
+            Node node4 = new Node(55);
+
             linkedList.AddNode(node1);
             linkedList.AddNode(node2);
             linkedList.AddNode(node3);
+            linkedList.AddNode(node4);
             linkedList.display();
         }
     }
