b5f0eb1ba69671db0b5f1df77f98cecec5c47105
mythili annamalai <mythiliannamalai45@gmail.com>
2022-03-29 16:26:52 +0530

    Creating node

--- LinkedList.cs
+++ LinkedList.cs
@@ -16,7 +16,7 @@ namespace Datastructure
             Tail = null;
         }
         public void AddNode(Node node)
-        {
+        {                        
             if (Head == null && Tail == null)
             {
                 Head = node;
@@ -25,34 +25,9 @@ namespace Datastructure
             else
             {
                 node.next = Head;
-                Head = node;
+                Head = node;              
             }
-        }
-        public void AddNodeFront(Node newNode)
-        {
-            newNode.next = Head;
-            Head = newNode;
-        }
-        public void AddNodeEnd(Node endNode)
-        {
-            endNode.next = Tail;
-            Tail = endNode;
-        }
-        public void AddNodeFrontSide(Node newNode1)
-        {
-            newNode1.next = Head;
-            Head = newNode1;
-        }
-        public void DeleteNodeatFirst()
-        {
-            if (this.Head == null)
-            {
-                Console.WriteLine("Nothing to delete list is empty");
-            }
-            Node temp = this.Head;
-            this.Head = this.Head.next;
-            Console.WriteLine("Removed from linkedlist" + temp.data);
-        }
+        }               
         public void display()
         {
             Node temp = Head;
@@ -61,28 +36,18 @@ namespace Datastructure
                 Console.WriteLine("Linked list is empty");
             }
             else
-            {
-                Console.WriteLine("Linked list as follows : ");
-                while (temp != null)
+            {                
+                while (temp!= null)
                 {
                     Console.Write(temp.data);
                     if (temp.next != null)
-                    {
+                    {                       
                         Console.Write("->");
                     }
-                    temp = temp.next;                                  
-                }
-                Node temp2 = Tail;
-                while (temp2 != null)
-                {
-                    if (temp2 != null)
-                    {
-                        Console.Write("->");
-                        Console.Write(temp2.data);
-                        temp2 = temp2.next;
-                    }
+                    temp = temp.next;
                 }
             }
+            
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -6,29 +6,20 @@ namespace Datastructure
         static void Main()
         {
             LinkedList linkedList = new LinkedList();
-            Node node1 = new Node(22);
-            Node node2 = new Node(23);
-            Node node3 = new Node(24);
+            Node node1 = new Node(56);
+            Node node2 = new Node(30);
+            Node node3 = new Node(70);
             linkedList.AddNode(node1);
             linkedList.AddNode(node2);
             linkedList.AddNode(node3);
             linkedList.display();
-            Console.WriteLine("\n Add new node in front");
-            Node newNode=new Node(55);
-            Node newNode2 = new Node(90);
-            linkedList.AddNodeFront(newNode);
-            linkedList.AddNodeFront(newNode2);
-            linkedList.display();
-            Console.WriteLine("\n Add new node in end");
-            Node endNode = new Node(16);
-            linkedList.AddNodeEnd(endNode);
-            linkedList.display();
-            Node newnode1=new Node(41);
-            linkedList.AddNodeFrontSide(newnode1);
-            linkedList.display();
-            Console.WriteLine("Delete the first node");
-            linkedList.DeleteNodeatFirst();
-            linkedList.display();
+
+            
+
+
+
+
+
         }
     }
 }
